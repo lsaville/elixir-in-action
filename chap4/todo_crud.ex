f67fc7b696ba3227c59eb5ce0a7d7a510c55de2a
Lee Saville <lee.saville@americanfinancing.net>
2019-05-03 22:02:56 -0600

    Complete crud functionality p121

--- chap4/todo_crud.ex
+++ chap4/todo_crud.ex
@@ -1,3 +1,4 @@
+
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
@@ -18,9 +19,38 @@ defmodule TodoList do
     }
   end
 
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: new_entries}
+  end
+
   def entries(todo_list, date) do
     todo_list.entries
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
 end
+
+todo_list =
+  TodoList.new() |>
+  TodoList.add_entry(%{date: ~D[2018-12-19], title: "Dentist"}) |>
+  TodoList.add_entry(%{date: ~D[2018-12-20], title: "Shopping"}) |>
+  TodoList.add_entry(%{date: ~D[2018-12-19], title: "Movies"})
+
+TodoList.entries(todo_list, ~D[2018-12-19])
