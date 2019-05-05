17f235972ae4a9155b11e8783954c5d51d427818
Lee Saville <lee.saville@americanfinancing.net>
2019-05-05 07:05:02 -0600

    Apply abbreviated function reference to my version

--- chap4/todo_builder.ex
+++ chap4/todo_builder.ex
@@ -62,76 +62,76 @@ defmodule TodoList do
 end
 
 # My version
-#defmodule TodoList.CsvImporter do
-#  def import(file_path) do
-#    File.stream!(file_path)
-#    |> Stream.map(&(String.replace(&1, "\n", "")))
-#    |> Stream.map(&(String.split(&1, ",")))
-#    |> Stream.map(&(prep_entry(&1)))
-#    |> Enum.to_list
-#    |> TodoList.new()
-#  end
-#
-#  defp prep_entry([date, title]) do
-#    # this seems a bit more straightforward to me
-#    #date = String.replace(date, "/", "-")
-#    #%{date: Date.from_iso8601(date), title: title}
-#
-#    [year, month, day] = String.split(date, "/")
-#    |> Enum.map(&(String.to_integer(&1)))
-#
-#    %{date: Date.new(year, month, day), title: title}
-#    #%{date: Date.from_erl({year, month, day}), title: title}
-#  end
-#end
+defmodule TodoList.CsvImporter do
+  def import(file_path) do
+    File.stream!(file_path)
+    |> Stream.map(&(String.replace(&1, "\n", "")))
+    |> Stream.map(&(String.split(&1, ",")))
+    |> Stream.map(&prep_entry/1)
+    |> Enum.to_list
+    |> TodoList.new()
+  end
+
+  defp prep_entry([date, title]) do
+    # this seems a bit more straightforward to me
+    #date = String.replace(date, "/", "-")
+    #%{date: Date.from_iso8601(date), title: title}
+
+    [year, month, day] = String.split(date, "/")
+    |> Enum.map(&String.to_integer/1)
+
+    %{date: Date.new(year, month, day), title: title}
+    #%{date: Date.from_erl({year, month, day}), title: title}
+  end
+end
 
 # Sasa version
 # Note top-level readability of import and create_entries.
 # Also use of &extract_fields/1 style in create_entries
-defmodule TodoList.CsvImporter do
-  def import(file_name) do
-    file_name
-    |> read_lines
-    |> create_entries
-    |> TodoList.new()
-  end
-
-  defp read_lines(file_name) do
-    file_name
-    |> File.stream!()
-    |> Stream.map(&(String.replace(&1, "\n", "")))
-  end
-
-  defp create_entries(lines) do
-    lines
-    |> Stream.map(&extract_fields/1)
-    |> Stream.map(&create_entry/1)
-  end
-
-  defp extract_fields(line) do
-    line
-    |> String.split(",")
-    |> convert_date
-  end
-
-  defp convert_date([date_string, title]) do
-    {parse_date(date_string), title}
-  end
-
-  defp parse_date(date_string) do
-    [year, month, day] =
-      date_string
-      |> String.split("/")
-      |> Enum.map(&String.to_integer/1)
-
-    {:ok, date} = Date.new(year, month, day)
-    date
-  end
-
-  defp create_entry({date, title}) do
-    %{date: date, title: title}
-  end
-end
+#defmodule TodoList.CsvImporter do
+#  def import(file_name) do
+#    file_name
+#    |> read_lines
+#    |> create_entries
+#    |> TodoList.new()
+#  end
+#
+#  defp read_lines(file_name) do
+#    file_name
+#    |> File.stream!()
+#    |> Stream.map(&(String.replace(&1, "\n", "")))
+#  end
+#
+#  defp create_entries(lines) do
+#    lines
+#    |> Stream.map(&extract_fields/1)
+#    |> Stream.map(&create_entry/1)
+#  end
+#
+#  defp extract_fields(line) do
+#    line
+#    |> String.split(",")
+#    |> convert_date
+#  end
+#
+#  defp convert_date([date_string, title]) do
+#    {parse_date(date_string), title}
+#  end
+#
+#  defp parse_date(date_string) do
+#    [year, month, day] =
+#      date_string
+#      |> String.split("/")
+#      |> Enum.map(&String.to_integer/1)
+#
+#    {:ok, date} = Date.new(year, month, day)
+#    date
+#  end
+#
+#  defp create_entry({date, title}) do
+#    %{date: date, title: title}
+#  end
+#end
 
 #Iex fodder
 
