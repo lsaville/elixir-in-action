3809de83e51f3fa12c95af60d276609ebd7bd368
Lee Saville <lee.saville@americanfinancing.net>
2019-05-08 05:34:43 -0600

    Update calculator based on book

--- chap5/calculator.ex
+++ chap5/calculator.ex
@@ -1,3 +1,51 @@
+# First crack
+#defmodule Calculator do
+#  def start do
+#    spawn(fn ->
+#      loop(0)
+#    end)
+#  end
+#
+#  def value(pid) do
+#    send(pid, {:value, self()})
+#
+#    receive do
+#      {:ok, value} -> value
+#    end
+#  end
+#
+#  def add(pid, num) do
+#    send(pid, {:add, num})
+#  end
+#
+#  def sub(pid, num) do
+#    send(pid, {:sub, num})
+#  end
+#
+#  def mul(pid, num) do
+#    send(pid, {:mul, num})
+#  end
+#
+#  def div(pid, num) do
+#    send(pid, {:div, num})
+#  end
+#
+#  defp loop(value) do
+#    receive do
+#      {:add, num} ->
+#        loop(value + num)
+#      {:sub, num} ->
+#        loop(value - num)
+#      {:mul, num} ->
+#        loop(value * num)
+#      {:div, num} ->
+#        loop(value / num)
+#      {:value, sender} ->
+#        send(sender, {:ok, value})
+#    end
+#  end
+#end
+
 defmodule Calculator do
   def start do
     spawn(fn ->
@@ -5,42 +53,35 @@ defmodule Calculator do
     end)
   end
 
-  def value(pid) do
-    send(pid, {:value, self()})
-    
+  def value(server_pid) do
+    send(server_pid, {:value, self()})
+
     receive do
-      {:ok, value} -> value
+      {:response, value} -> value
     end
   end
 
-  def add(pid, num) do
-    send(pid, {:add, num})
-  end
+  def add(server_pid, num), do: send(server_pid, {:add, num})
+  def sub(server_pid, num), do: send(server_pid, {:sub, num})
+  def mul(server_pid, num), do: send(server_pid, {:mul, num})
+  def div(server_pid, num), do: send(server_pid, {:div, num})
 
-  def sub(pid, num) do
-    send(pid, {:sub, num})
-  end
+  defp loop(current_value) do
+    new_value =
+      receive do
+        {:value, caller} ->
+          send(caller, {:response, current_value})
+          current_value
+        {:add, num} -> current_value + num
+        {:sub, num} -> current_value - num
+        {:mul, num} -> current_value * num
+        {:div, num} -> current_value / num
 
-  def mul(pid, num) do
-    send(pid, {:mul, num})
-  end
+        invalid_request ->
+          IO.puts("invalid request #{inspect invalid_request}")
+          current_value
+      end
 
-  def div(pid, num) do
-    send(pid, {:div, num})
-  end
-
-  defp loop(value) do
-    receive do
-      {:add, num} ->
-        loop(value + num)
-      {:sub, num} ->
-        loop(value - num)
-      {:mul, num} ->
-        loop(value * num)
-      {:div, num} ->
-        loop(value / num)
-      {:value, sender} ->
-        send(sender, {:ok, value})
-    end
+    loop(new_value)
   end
 end
