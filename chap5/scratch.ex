1ba2ace1b53eb743fc9d00154852eb1fbd06fd54
Lee Saville <lee.saville@americanfinancing.net>
2019-05-07 19:45:59 -0600

    Add bits

--- chap5/scratch.ex
+++ chap5/scratch.ex
@@ -12,3 +12,58 @@ async_query =
   end
 
 Enum.each(1..5, &async_query.("query #{&1}"))
+
+# synchronous sending
+run_query =
+  fn query_def ->
+    Process.sleep(2000)
+    "#{query_def} result"
+  end
+
+async_query =
+  fn query_def ->
+    caller = self()
+    spawn(fn ->
+      send(caller, {:query_result, run_query.(query_def)})
+    end)
+  end
+
+get_result =
+  fn ->
+    receive do
+      {:query_result, result} -> result
+    end
+  end
+
+results = Enum.map(1..5, fn _ -> get_result.() end)
+
+1..5 |>
+Enum.map(&async_query.("query #{&1}")) |>
+Enum.map(fn _ -> get_result.() end)
+
+
+server_pid = DatabaseServer.start()
+DatabaseServer.run_async(server_pid, "query 1")
+DatabaseServer.get_result()
+DatabaseServer.run_async(server_pid, "query 2")
+DatabaseServer.get_result()
+
+pool = Enum.map(1..100, fn _ -> DatabaseServer.start() end))
+
+Enum.each(
+  1..5,
+  fn query_def ->
+    server_pid = Enum.at(pool, :rand.uniform(100) - 1)
+    DatabaseServer.run_async(server_pid, query_def)
+  end
+)
+
+Enum.map(1..5, fn _ -> DatabaseServer.get_result() end)
+
+# For Calculator
+pid = Calculator.start()
+Calculator.add(pid, 10)
+Calculator.sub(pid, 5)
+Calculator.mul(pid, 3)
+Calculator.div(pid, 5)
+Calculator.value(pid)
