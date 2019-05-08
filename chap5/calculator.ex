b2c0c648dee0c2509c44ed9984272e0af8619989
Lee Saville <lee.saville@americanfinancing.net>
2019-05-08 05:46:25 -0600

    Refactor calculator

--- chap5/calculator.ex
+++ chap5/calculator.ex
@@ -69,19 +69,22 @@ defmodule Calculator do
   defp loop(current_value) do
     new_value =
       receive do
-        {:value, caller} ->
-          send(caller, {:response, current_value})
-          current_value
-        {:add, num} -> current_value + num
-        {:sub, num} -> current_value - num
-        {:mul, num} -> current_value * num
-        {:div, num} -> current_value / num
-
-        invalid_request ->
-          IO.puts("invalid request #{inspect invalid_request}")
-          current_value
+        message -> process_message(current_value, message)
       end
 
     loop(new_value)
+  end
+
+  defp process_message(current_value, {:value, caller}) do
+    send(caller, {:response, current_value})
+    current_value
+  end
+  defp process_message(current_value, {:add, value}), do: current_value + value
+  defp process_message(current_value, {:sub, value}), do: current_value - value
+  defp process_message(current_value, {:mul, value}), do: current_value * value
+  defp process_message(current_value, {:div, value}), do: current_value / value
+  defp process_message(current_value, invalid_request) do
+    IO.puts("invalid request #{inspect invalid_request}")
+    current_value
   end
 end
