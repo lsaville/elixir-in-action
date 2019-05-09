2f4b118bc920958e439030ffca29a35c03fcb3c7
Lee Saville <lee.saville@americanfinancing.net>
2019-05-09 06:05:47 -0600

    Add copy pasta fodder

--- chap5/scratch.ex
+++ chap5/scratch.ex
@@ -67,3 +67,10 @@ Calculator.sub(pid, 5)
 Calculator.mul(pid, 3)
 Calculator.div(pid, 5)
 Calculator.value(pid)
+
+# For TodoServer
+todo_server = TodoServer.start()
+TodoServer.add_entry(todo_server, %{date: ~D[2018-12-19], title: "Dentist"})
+TodoServer.add_entry(todo_server, %{date: ~D[2018-12-20], title: "Shopping"})
+TodoServer.add_entry(todo_server, %{date: ~D[2018-12-19], title: "Movies"})
+TodoServer.entries(todo_server, ~D[2018-12-19])
