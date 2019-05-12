923aa9ffca81ea658a7eacf3c745898247e3259b
Lee Saville <lee.saville@americanfinancing.net>
2019-05-12 09:15:55 -0600

    Make client oblivious to server

--- chap6/server_process.ex
+++ chap6/server_process.ex
@@ -31,9 +31,23 @@ defmodule ServerProcess do
   end
 end
 
+# server oblivious client
 defmodule KeyValueStore do
   def init do
     %{}
+  end
+
+  def start do
+    ServerProcess.start(KeyValueStore)
+    #ServerProcess.start(__MODULE__)
+  end
+
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
   end
 
   def handle_call({:put, key, value}, state) do
@@ -44,3 +58,18 @@ defmodule KeyValueStore do
     {Map.get(state, key), state}
   end
 end
+
+# first version
+#defmodule KeyValueStore do
+#  def init do
+#    %{}
+#  end
+#
+#  def handle_call({:put, key, value}, state) do
+#    {:ok, Map.put(state, key, value)}
+#  end
+#
+#  def handle_call({:get, key}, state) do
+#    {Map.get(state, key), state}
+#  end
+#end
