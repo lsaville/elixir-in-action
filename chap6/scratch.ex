1460a21225aa28ef0cb401217eb4e06cfc230013
Lee Saville <lee.saville@americanfinancing.net>
2019-05-12 09:16:30 -0600

    Update for server oblivious style

--- chap6/scratch.ex
+++ chap6/scratch.ex
@@ -1,3 +1,7 @@
 pid = ServerProcess.start(KeyValueStore)
 ServerProcess.call(pid, {:put, :some_key, :some_value})
 ServerProcess.call(pid, {:get, :some_key})
+
+pid = KeyValueStore.start()
+KeyValueStore.put(pid, :some_key, :some_value)
+KeyValueStore.get(pid, :some_key)
