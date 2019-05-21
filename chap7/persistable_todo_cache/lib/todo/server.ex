e620724c3be38c304bbe9ad0169dd4ff26870af9
Lee Saville <lee.saville@americanfinancing.net>
2019-05-21 05:50:29 -0600

    Update to handle name

--- chap7/persistable_todo_cache/lib/todo/server.ex
+++ chap7/persistable_todo_cache/lib/todo/server.ex
@@ -1,8 +1,8 @@
 defmodule Todo.Server do
   use GenServer
 
-  def start do
-    GenServer.start(Todo.Server, nil)
+  def start(name) do
+    GenServer.start(Todo.Server, name)
   end
 
   def add_entry(pid, entry) do
@@ -28,38 +28,46 @@ defmodule Todo.Server do
   end
 
   @impl GenServer
-  def init(_) do
-    {:ok, Todo.List.new()}
+  def init(name) do
+    {:ok, {name, Todo.List.new()}}
   end
 
 
   @impl GenServer
-  def handle_cast({:add_entry, entry}, todo_list) do
-    {:noreply, Todo.List.add_entry(todo_list, entry)}
+  def handle_cast({:add_entry, entry}, {name, todo_list}) do
+    new_list = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
   end
 
   @impl GenServer
-  def handle_cast({:update_entry, entry}, todo_list) do
-    {:noreply, Todo.List.update_entry(todo_list, entry)}
+  def handle_cast({:update_entry, entry}, {name, todo_list}) do
+    new_list = Todo.List.update_entry(todo_list, entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
   end
 
   @impl GenServer
-  def handle_cast({:update_entry, entry_id, updater_fun}, todo_list) do
-    {:noreply, Todo.List.update_entry(todo_list, entry_id, updater_fun)}
+  def handle_cast({:update_entry, entry_id, updater_fun}, {name, todo_list}) do
+    new_list = Todo.List.update_entry(todo_list, entry_id, updater_fun)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
   end
 
   @impl GenServer
-  def handle_cast({:delete_entry, entry_id}, todo_list) do
-    {:noreply, Todo.List.delete_entry(todo_list, entry_id)}
+  def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
+    new_list = Todo.List.delete_entry(todo_list, entry_id)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
   end
 
   @impl GenServer
-  def handle_call({:entries, date}, _from, todo_list) do
-    {:reply, Todo.List.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _from, {name, todo_list}) do
+    {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
   end
 
   @impl GenServer
-  def handle_call({:entries}, _from, todo_list) do
-    {:reply, Todo.List.entries(todo_list), todo_list}
+  def handle_call({:entries}, _from, {name, todo_list}) do
+    {:reply, Todo.List.entries(todo_list), {name, todo_list}}
   end
 end
