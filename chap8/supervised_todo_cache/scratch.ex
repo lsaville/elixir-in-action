c7ed68311eaf37716d943eedf37b861bda935a38
Lee Saville <lee.saville@americanfinancing.net>
2019-05-30 06:27:22 -0600

    Add scratch notes

--- chap8/supervised_todo_cache/scratch.ex
+++ chap8/supervised_todo_cache/scratch.ex
@@ -1,1 +1,29 @@
 Supervisor.start_link([Todo.Cache], strategy: :one_for_one)
+bobs_list = Todo.Cache.server_process("Bob's list")
+cache_pid = Process.whereis(Todo.Cache)
+Process.exit(cache_pid, :kill)
+Process.whereis(Todo.Cache)
+
+Supervisor.start_link(
+  [
+    %{
+      id: Todo.Cache,
+      start: {Todo.Cache, :start_link, [nil]}
+    }
+  ],
+  strategy: :one_for_one
+)
+
+Supervisor.start_link(
+  [{Todo.Cache, nil}],
+  strategy: :one_for_one
+)
+
+Supervisor.start_link([Todo.Cache], strategy: :one_for_one)
+# ^^^ 7-22 are various permutations of the same thing
+
+Todo.Cache.child_spec(nil)
+# This should give back
+# %{id: Todo.Cache, start: {Todo.Cache, :start_link, [nil]}}
+
+Todo.System.start_link()
