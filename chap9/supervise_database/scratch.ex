8bd6ed585ab02e8443d0edc15fa81569914468eb
Lee Saville <lee.saville@americanfinancing.net>
2019-06-05 05:56:47 -0600

    Add EchoServer demo

--- chap9/supervise_database/scratch.ex
+++ chap9/supervise_database/scratch.ex
@@ -35,3 +35,34 @@ Registry.lookup(Registry.UniqueLookupTest, "hello")
 Task.async(fn ->
   Registry.lookup(Registry.UniqueLookupTest, "hello")
 end) |> Task.await()
+
+# EchoServer
+defmodule EchoServer do
+  use GenServer
+
+  def start_link(id) do
+    GenServer.start_link(__MODULE__, nil, name: via_tuple(id))
+  end
+
+  def call(id, some_request) do
+    GenServer.call(via_tuple(id), some_request)
+  end
+
+  defp via_tuple(id) do
+    {:via, Registry, {:my_registry, {__MODULE__, id}}}
+  end
+
+  def handle_call(some_request, _, state) do
+    IO.inspect(self())
+    {:reply, some_request, state}
+  end
+end
+
+Registry.start_link(name: :my_registry, keys: :unique)
+
+EchoServer.start_link("server one")
+EchoServer.start_link("server two")
+
+EchoServer.call("server one", :some_request)
+EchoServer.call("server two", :another_request)
+
